fe6731dcdebb5d8bc8a973234d1dc90930c736ca
ms <maemotosato@gmail.coms>
2024-07-31 23:58:30 +0900

    update

---
+++ front/src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi: {
+        component: 'mdi',
+      },
+    },
+  },
+})
